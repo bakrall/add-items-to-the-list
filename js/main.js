7fb99bac7a6f27fe58b07869eb4b68b1e6b44c91
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-05-24 21:54:40 +0200

    populate list with added items

--- js/main.js
+++ js/main.js
@@ -2,17 +2,35 @@ const addItems = document.querySelector('.add-items'),
 	itemsList = document.querySelector('.plates'),
 	items = [];
 
-	function addItem(e) {
-		e.preventDefault(); //stop the page from reloading on submit
-		const text = this.querySelector('[name=item]').value;
+function addItem(e) {
+	e.preventDefault(); //stop the page from reloading on submit
+	const text = this.querySelector('[name=item]').value;
 
-		const item = {
-			text, //shorthand for text: text
-			done: false //whether item is ticked
-		}
-
-		this.reset();
+	const item = {
+		text, //shorthand for text: text
+		done: false //whether item is ticked
 	}
 
-	addItems.addEventListener('submit', addItem);
+	items.push(item);
+	populateList(items, itemsList);
+	this.reset();
+}
+
+/* 
+we set default "plates" as empty array - if we forget to pass in something, it is not gonna break js, but it is gonna 
+loop over empty array 
+
+ */
+function populateList(plates = [], platesList) {
+	platesList.innerHTML = plates.map((plate, i) => {
+		return `
+			<li>
+				<input type="checkbox" data-index=${i} id="item${i}" />
+				<label for="item${i}">${plate.text}</label>
+			</li>
+		`;
+	}).join('');
+}
+
+addItems.addEventListener('submit', addItem);
 
